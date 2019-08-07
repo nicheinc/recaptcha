d7a1bfb5f2373b5df3e2a75e7b5cd8043f399991
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-08-07 10:43:36 -0400

    More robust example server

--- example/main.go
+++ example/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net"
@@ -51,31 +52,50 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := tmpl.Execute(w, data); err != nil {
-		log.Fatalf("Error executing template: %s", err)
+		http.Error(w, fmt.Sprintf("Error executing template: %s", err), http.StatusInternalServerError)
+		return
 	}
 }
 
 func submitHandler(w http.ResponseWriter, r *http.Request) {
 	token := r.FormValue("g-recaptcha-response")
 	if token == "" {
-		log.Fatalln("Form submission missing g-recaptcha-response")
+		http.Error(w, "Form submission missing g-recaptcha-response parameter", http.StatusBadRequest)
+		return
 	}
 	userIP, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
-		log.Fatalf("Error parsing remote addr: %s", err)
+		http.Error(w,
+			fmt.Sprintf("Error parsing remote addr: %s", err),
+			http.StatusBadRequest,
+		)
+		return
 	}
 
 	response, err := client.Fetch(context.Background(), token, userIP)
 	if err != nil {
-		log.Fatalf("Error making request to token verification endpoint: %s", err)
+		http.Error(w,
+			fmt.Sprintf("Error making request to token verification endpoint: %s", err),
+			http.StatusInternalServerError,
+		)
+		return
 	}
 
 	out, err := json.MarshalIndent(response, "", "    ")
 	if err != nil {
-		log.Fatalf("Error marshalling verification endpoint response: %s\n", err)
+		http.Error(w,
+			fmt.Sprintf("Error marshalling verification endpoint response: %s\n", err),
+			http.StatusInternalServerError,
+		)
+		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	if _, err := w.Write(out); err != nil {
-		log.Fatalf("Error writing response: %s\n", err)
+		http.Error(w,
+			fmt.Sprintf("Error writing response: %s\n", err),
+			http.StatusInternalServerError,
+		)
+		return
 	}
 }
